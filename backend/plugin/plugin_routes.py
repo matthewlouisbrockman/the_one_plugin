c0adfaa76541e6d251aab0d4b876c1a4d3e5ff03
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 13:10:41 -0700

    let's see if auth works

--- backend/plugin/plugin_routes.py
+++ backend/plugin/plugin_routes.py
@@ -1,9 +1,20 @@
 from flask import Blueprint, request, send_from_directory, Response
 from config import IS_LOCALHOST
 import yaml
+import dotenv
+from os import getenv
+dotenv.load_dotenv()
+
 
 bp = Blueprint("plugin", __name__)
 print('localhost started? ', IS_LOCALHOST)
+
+AUTHO_CLIENT_URL = getenv('AUTHO_CLIENT_URL')
+AUTHO_AUTHORIZATION_URL = getenv('AUTHO_AUTHORIZATION_URL')
+OPENAI_VERIFICATION_TOKEN = getenv('OPENAI_VERIFICATION_TOKEN')
+
+if not (AUTHO_CLIENT_URL and AUTHO_AUTHORIZATION_URL and OPENAI_VERIFICATION_TOKEN):
+    print('WARNING: THIS WILL NOT WORK ON PRODUCTION WITHOUT AUTHO_CLIENT_URL, AUTHO_AUTHORIZATION_URL, and OPENAI_VERIFICATION_TOKEN ENV VARIABLES SET')
 
 @bp.route("/.well-known/ai-plugin.json", methods=["GET"])
 def get_ai_plugin():
@@ -18,6 +29,10 @@ def get_ai_plugin():
         with open('./plugin/manifest.json', 'r') as f:
             text = f.read()
             text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+            text = text.replace("AUTHO_CLIENT_URL", AUTHO_CLIENT_URL)
+            text = text.replace("AUTHO_AUTHORIZATION_URL", AUTHO_AUTHORIZATION_URL)
+            text = text.replace("OPENAI_VERIFICATION_TOKEN", OPENAI_VERIFICATION_TOKEN)
+
     return Response(text, mimetype="text/json")
 
 @bp.route("/openapi.yaml", methods=["GET"])
