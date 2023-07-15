5aded3a94cde0a099cc9fb27fcd1d769234b4320
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 15:09:18 -0700

    create job button

---
+++ top_frontend/components/app/management.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export const Management = () => {
+  const [jobs, setJobs] = useState([]);
+  const [currentJob, setCurrentJob] = useState(null);
+  const [jobTasks, setJobTasks] = useState([]);
+
+  return (
+    <div className="flex flex-col w-full h-full bg-black text-white">
+      <button className="bg-white text-black rounded px-3 py-1">
+        Create Job
+      </button>
+      <JobSelectionDropdown
+        jobs={jobs}
+        currentJob={currentJob}
+        setCurrentJob={setCurrentJob}
+      />
+    </div>
+  );
+};
+
+const JobSelectionDropdown = ({ jobs, currentJob, setCurrentJob }) => {
+  return (
+    <div className="flex flex-row">
+      {!!jobs?.length && (
+        <select
+          value={currentJob}
+          onChange={(e) => setCurrentJob(e.target.value)}
+        >
+          {jobs.map((job) => (
+            <option key={job.id} value={job.id}>
+              {job.name}
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+};
--- top_frontend/pages/index.js
+++ top_frontend/pages/index.js
@@ -1,10 +1,14 @@
 import { HomePage } from "@/components/app/homepage";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { useEffect } from "react";
+import { Management } from "@/components/app/management";
 const Index = () => {
   const { user } = useUser();
-
-  return <div>{!user && <HomePage />}</div>;
+  return (
+    <div>
+      {!user && <HomePage />}
+      {user && <Management />}
+    </div>
+  );
 };
 
 export default Index;
