385219ab938bc280b637264b6eb2415321d68eea
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 15:31:41 -0700

    can add jobs maybe

--- backend/management/management_routes.py
+++ backend/management/management_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, g
+from flask import Blueprint, jsonify, g, request
 from wrappers.auth_required import auth_required
 from models.jobs import TOPJob
 bp = Blueprint("management", __name__, url_prefix="/management")
@@ -12,8 +12,17 @@ def get_jobs():
 
     print('jobs: ', jobs)
 
-    return jsonify({'jobs': [{
-        'id': 1,
-        'name': 'job1',
-    }]})
+    return jsonify({'jobs': [job.serialize() for job in jobs]})
 
+@bp.route("/jobs", methods=["POST"])
+@auth_required
+def create_job():
+    payload = request.get_json()
+
+    job_name = payload.get('job_name')
+    job_description = payload.get('job_description')
+
+    job = TOPJob(job_name, job_description, g.user_id)
+    job.save()
+
+    return jsonify({'job': job.serialize()})
