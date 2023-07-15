ea6714ef54984f5dce4e49483b691494bed74d8c
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 15:17:08 -0700

    auth wrapper works

--- backend/management/management_routes.py
+++ backend/management/management_routes.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, g
+from wrappers.auth_required import auth_required
 
 bp = Blueprint("management", __name__, url_prefix="/management")
 
 @bp.route("/jobs", methods=["GET"])
+@auth_required
 def get_jobs():
+    print('user info: ', g.user_id)
     return jsonify({'jobs': [{
         'id': 1,
         'name': 'job1',
