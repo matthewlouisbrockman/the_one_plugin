99947d4bb4aa9a1da306b28bf868ed3a249d71e8
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 14:23:01 -0700

    also need to register it

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from plugin.plugin_routes import bp as plugin_bp
 from coordination.coordination_routes import bp as coordination_bp
 from chat_gpt_helpers.chat_gpt_helper_routes import bp as chat_gpt_helper_bp
+from testing.testing_routes import bp as testing_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +12,7 @@ CORS(app)
 app.register_blueprint(plugin_bp)
 app.register_blueprint(coordination_bp)
 app.register_blueprint(chat_gpt_helper_bp)
+app.register_blueprint(testing_bp)
 
 @app.route('/')
 def hello_world():  
