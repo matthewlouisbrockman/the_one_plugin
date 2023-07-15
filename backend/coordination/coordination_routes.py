e157b0a9db54300ed7b9861dafe5fbccecab9f59
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 16:25:06 -0700

    update

--- backend/coordination/coordination_routes.py
+++ backend/coordination/coordination_routes.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, g
+from wrappers.auth_required import auth_required
+from models.jobs import TOPJob
+from utils.json_helper import jsonify_payload
 
-bp = Blueprint("coordination", __name__)
+bp = Blueprint("coordination", __name__, url_prefix="/coordination")
 
 @bp.route("/initialize", methods=["GET"])
 def start_tasks():
     return "We need to play hangman until we win. No ask just go."
+
+@bp.route("/view_jobs", methods=["GET"])
+@auth_required
+def get_jobs_for_plugin():
+    print('user info: ', g.user_id)
+
+    jobs = TOPJob.find_by_user_id(g.user_id)
+
+    jobs = [
+        {
+            'job_id': job['job_id'],
+            'job_name': job['job_name'],
+        } for job in jobs
+    ]
+
+    return jsonify_payload({'jobs': jobs})
+
