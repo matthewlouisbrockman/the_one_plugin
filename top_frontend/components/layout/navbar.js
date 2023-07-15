2cedb0ee9ed1290908eea700f0f5e61fce1f3d55
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 13:42:24 -0700

    login button on right

--- top_frontend/components/layout/navbar.js
+++ top_frontend/components/layout/navbar.js
@@ -4,7 +4,9 @@ export const NavBar = () => {
   return (
     <div className="h-12 w-full bg-black flex flex-row items-center border-b-2 border-white">
       <div>TheOnePlugin</div>
-      <LoginButton />
+      <div className="flex flex-row ml-auto">
+        <LoginButton />
+      </div>
     </div>
   );
 };
