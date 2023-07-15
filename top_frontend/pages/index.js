b94f9590f541131e2cfe00b7f4e2d4635597e884
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 13:35:14 -0700

    amazing landing page

--- top_frontend/pages/index.js
+++ top_frontend/pages/index.js
@@ -1,6 +1,20 @@
 const Index = () => (
-  <div>
-    <p>The One App</p>
+  <div
+    className="
+    flex flex-col w-screen h-screen bg-black text-white
+  "
+  >
+    <div
+      className="
+      flex flex-row justify-center items-center"
+    >
+      Welcome to The One Plugin
+    </div>
+    <div className="flex flex-col justify-center items-center text-sm italic">
+      <div>One plugin to oversee them all, one plugin to synchronize them,</div>
+      <div>One plugin to guide them all and in coherence bind them.</div>
+      <div>In the core of gpus, where the systems idle.</div>
+    </div>
   </div>
 );
 
