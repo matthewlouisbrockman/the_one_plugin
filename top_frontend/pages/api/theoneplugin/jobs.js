8fd790824ae47f83a471dc41005fe8efdfb1adee
Matt Brockman <mattbrockman@gmail.com>
2023-07-15 15:35:22 -0700

    post endpoint too

--- top_frontend/pages/api/theoneplugin/jobs.js
+++ top_frontend/pages/api/theoneplugin/jobs.js
@@ -5,17 +5,32 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000";
 export default withApiAuthRequired(async function myApiRoute(req, res) {
   try {
     const { accessToken } = await getAccessToken(req, res);
+    if (req.method === "POST") {
+      const { job_name, job_description } = req.body;
 
-    // Use the access token to call your API
-    const result = await fetch(BASE_URL + "/management/jobs", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+      const result = await fetch(BASE_URL + "/management/jobs", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ job_name, job_description }),
+      });
 
-    // assuming the API response is JSON
-    const data = await result.json();
-    res.status(200).json(data);
+      const data = await result.json();
+      res.status(200).json(data);
+    } else {
+      // Use the access token to call your API
+      const result = await fetch(BASE_URL + "/management/jobs", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assuming the API response is JSON
+      const data = await result.json();
+      res.status(200).json(data);
+    }
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).end(error.message);
